720aeb6413749468192b3c1b121f7e69aa0fba79
panguclick <hook1982@gmail.com>
2022-11-18 18:44:30 +0800

    初始化base库

---
+++ allocator/partition_allocator/partition_alloc_base/migration_adapter.h
@@ -0,0 +1,66 @@
+// Copyright (c) 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BASE_ALLOCATOR_PARTITION_ALLOCATOR_PARTITION_ALLOC_BASE_MIGRATION_ADAPTER_H_
+#define BASE_ALLOCATOR_PARTITION_ALLOCATOR_PARTITION_ALLOC_BASE_MIGRATION_ADAPTER_H_
+
+#include <cstdint>
+#include <string>
+
+#include "build/build_config.h"
+
+#if BUILDFLAG(IS_MAC)
+#include <CoreFoundation/CoreFoundation.h>
+#endif
+
+namespace base {
+
+class LapTimer;
+
+template <typename Type, typename Traits>
+class LazyInstance;
+
+template <typename Type>
+struct LazyInstanceTraitsBase;
+
+#if BUILDFLAG(IS_ANDROID)
+template <typename CharT, typename Traits>
+class BasicStringPiece;
+using StringPiece = BasicStringPiece<char, std::char_traits<char>>;
+#endif
+
+#if BUILDFLAG(IS_MAC)
+
+namespace mac {
+
+template <typename T>
+T CFCast(const CFTypeRef& cf_val);
+template <typename T>
+T CFCastStrict(const CFTypeRef& cf_val);
+
+}  // namespace mac
+
+#endif  // BUILDFLAG(IS_MAC)
+
+}  // namespace base
+
+namespace partition_alloc::internal::base {
+
+// TODO(https://crbug.com/1288247): Remove these 'using' declarations once
+// the migration to the new namespaces gets done.
+using ::base::LapTimer;
+using ::base::LazyInstance;
+using ::base::LazyInstanceTraitsBase;
+
+#if BUILDFLAG(IS_MAC)
+namespace mac {
+
+using ::base::mac::CFCast;
+
+}  // namespace mac
+#endif  // BUILDFLAG(IS_MAC)
+
+}  // namespace partition_alloc::internal::base
+
+#endif  // BASE_ALLOCATOR_PARTITION_ALLOCATOR_PARTITION_ALLOC_BASE_MIGRATION_ADAPTER_H_
